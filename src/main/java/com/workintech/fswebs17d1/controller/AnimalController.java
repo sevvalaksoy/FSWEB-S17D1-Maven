05966141f179edb096c05ea2926060d58f8c0de0
sevvalaksoy <164859994+sevvalaksoy@users.noreply.github.com>
2024-08-19 13:29:09 +0300

    almost

--- src/main/java/com/workintech/fswebs17d1/controller/AnimalController.java
+++ src/main/java/com/workintech/fswebs17d1/controller/AnimalController.java
@@ -1,12 +1,41 @@
 package com.workintech.fswebs17d1.controller;
 
 import com.workintech.fswebs17d1.entity.Animal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-@RestController("/workintech")
+@RestController
+@RequestMapping("/workintech/animal")
 public class AnimalController {
-    private Map<Integer, Animal> animals;
+    private Map<Integer, Animal> animals = new HashMap<>();
+
+
+    @GetMapping("/")
+    public List<Animal> getData(){
+        return animals.values().stream().toList();
+    }
+
+    @GetMapping("/{id}")
+    public Animal getAnimal(@PathVariable int id){
+            return animals.get(id);
+    }
+
+    @PostMapping("/")
+    public void addAnimal(String name, int id){
+        Animal animal = new Animal(id, name);
+        animals.put(id, animal);
+    }
+
+    @PutMapping("/{id}")
+    public void update(@PathVariable int id, @RequestBody Animal animal){
+        animals.put(id, animal);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id){
+        animals.remove(id);
+    }
 }
--- src/main/java/com/workintech/fswebs17d1/entity/Animal.java
+++ src/main/java/com/workintech/fswebs17d1/entity/Animal.java
@@ -4,4 +4,19 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Animal {
+    private String name;
+    private Integer id;
+    public Animal(Integer id, String name){
+        this.name = name;
+        this.id = id;
+    }
+    public Animal(){
+    }
+
+    public String getName(){
+        return name;
+    }
+    public Integer getId(){
+        return id;
+    }
 }
